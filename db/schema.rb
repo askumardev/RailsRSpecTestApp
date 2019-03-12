b0e0c407295fe9ff7f66eebe9d8c4fdc2e52cceb
satish <asatish764@gmail.com>
2019-03-12 18:25:45 +0530

    Article spec created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190312094253) do
+ActiveRecord::Schema.define(version: 20190312123937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
---
+++ spec/features/creating_article_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.feature "Creating Articles" do
+	scenario "A user creates a new article" do
+
+		visit "/"
+
+		click_link "New Article"
+		fill_in "Title", with: "Creating new blog"
+		fill_in "Body", with: "Blog Body"
+		click_button "Create Article"
+
+		expect(page).to have_content("Article has been created")
+		expect(page.current_path).to eq(articles_path)
+	end
+end
