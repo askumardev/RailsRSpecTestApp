8518f65887b4bbf9de7960903450557670a1b9a1
sk534 <asatish764@gmail.com>
2021-01-19 13:37:54 +0530

    new updates

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   resources :users
   resources :articles
-  root to: "articles#index"
+  #root to: "articles#index"
+  root to: "users#index"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
